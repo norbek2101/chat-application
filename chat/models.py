02474513570e483fe08fdc049e68cfee88b2f57a
norbek2101 <lightnight1223@gmail.com>
2022-10-05 11:23:25 +0500

    add Message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='messages')
+    chat = models.IntegerField()
+    text = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+
