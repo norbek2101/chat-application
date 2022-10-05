87835eec3036afb5d0ba4e5d7c9bbe837b541459
norbek2101 <lightnight1223@gmail.com>
2022-10-05 12:45:54 +0500

    add path for MessageView and MessageDeatilView

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from chat import views
 urlpatterns = [
-    
+    path('', views.MessageView.as_view()),
+    path('chat-detail/<int:pk>/', views.MessageDetailView.as_view()),
 ]
