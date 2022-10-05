3187712b0be89eecf332e116f335e82dafd90a03
norbek2101 <lightnight1223@gmail.com>
2022-10-05 13:09:55 +0500

    feat: configure asgi to channels

--- config/asgi.py
+++ config/asgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 
-application = get_asgi_application()
+
+from config.middleware import JWTAuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
+
+
+application = ProtocolTypeRouter(
+        {
+            "http": get_asgi_application(),
+            "websocket": JWTAuthMiddlewareStack(
+                                URLRouter(websocket_urlpatterns)
+                                )
+        }
+    )
