b1dab6d1bdd0f3a4d588d95d1cc1e06fda3f8a8f
norbek2101 <lightnight1223@gmail.com>
2022-10-05 12:47:47 +0500

    add MessageView and MessageDetailView

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import  generics, permissions
 
-# Create your views here.
+from chat.models import Message
+from chat.serializers import MessageSerializer
+
+
+class MessageView(generics.ListCreateAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.AllowAny]
+    queryset = Message.objects.all()
+
+
+class MessageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.AllowAny]
+    queryset = Message.objects.all()
