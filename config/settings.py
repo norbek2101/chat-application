6eb49b849a3ddaf9fcf0053e8ef4513e54dcd29d
norbek2101 <lightnight1223@gmail.com>
2022-10-05 13:08:17 +0500

    feat: add swagger settings

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -162,3 +163,20 @@ REST_USE_JWT = True
 
 JWT_AUTH_COOKIE = 'chat-auth'
 JWT_AUTH_REFRESH_COOKIE = 'chat-refresh-token'
+
+
+SWAGGER_SETTINGS = {
+   'USE_SESSION_AUTH': False,
+   'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'in': 'header',
+            'name': 'Authorization',
+            'type': 'apiKey',
+        },
+    }
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=15)
+}
